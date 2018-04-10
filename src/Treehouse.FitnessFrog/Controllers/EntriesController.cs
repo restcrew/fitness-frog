4eb56eda9dff5a35322ea75c3411beb792829379
Risto Jauhiainen <risto.jauhiainen@gmail.com>
2018-04-10 15:52:55 +0300

    bootstrap validation

--- src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -43,9 +43,10 @@ namespace Treehouse.FitnessFrog.Controllers
         {
             var entry = new Entry()
             {
-                Date = DateTime.Today,     
+                Date = DateTime.Today,
+                
             };
-
+            
             ViewBag.ActivitiesSelectListItems = new SelectList(
                 Data.Data.Activities, "Id", "Name");
 
